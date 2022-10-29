000b280194bf23880772482d375dffddb2682587
RoseyV <115703166+RoseyV@users.noreply.github.com>
2022-10-29 13:14:22 +0800

    Create UniqueID.java

---
+++ UniqueID.java
@@ -0,0 +1,29 @@
+package p3project;
+
+public class UniqueID 
+
+{
+
+    private int uid;
+
+    
+
+    UniqueID(int id)
+
+    {
+
+        this.uid = id;
+
+    }
+
+    
+
+    public int getuid()
+
+    {
+
+        return uid;
+
+    }
+
+}
